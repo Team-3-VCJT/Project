1f51ded48ae215c6d7db7928a49a3398d53a6611
Thomas Frohm <tfrohm11@mail.com>
2022-01-27 20:31:07 -0600

    label commit

--- assets/script.js
+++ assets/script.js
@@ -1,78 +1,127 @@
 // Global Variables
 const labels = [];
 const graphData = [];
+const graphSecondData = [];
+
+var dataOneLabel = "";
+var dataTwoLabel = "";
+
+
 // Functions
 
-// get the cream filling
 
-// fetch url
-function displayResults(results) {
-    results.forEach(result => {
-        console.log(result.state, result.actuals.cases);
-        labels.push(result.state);
-        graphData.push(result.actuals.cases);
-        document.getElementById("graph").innerHTML += `${result.state}:${result.actuals.cases}`
-    })
+// Event Listeners
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: dataOneLabel,
+      backgroundColor: "rgb(255, 99, 132)",
+      data: graphData,
+    },
+    {
+      label: dataTwoLabel,
+      backgroundColor: "blue",
+      data: graphSecondData,
+    },
+  ],
+};
+
+const config = {
+  type: "bar",
+  data: data,
+  options: {},
+};
+var myChart = new Chart(document.getElementById("myChart"), config);
+// drop down menu functions
+
+// dropdown open/close function
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((item) => {
+  item.addEventListener("click", function (event) {
+    event.stopPropagation();
+    item.classList.toggle("is-active");
+  });
+});
+// select data function
+document.querySelectorAll(".dropdown-item").forEach((item) => {
+  item.addEventListener("click", function (event) {
+    event.stopPropagation();
+    item.classList.toggle("is-active");
+  });
+});
+
+// handle data function// add if second data field has no selection, don't run
+function handleAllData() {
+  handleHistoricalData();
+  handleSecondData();
 }
-
-function displayStateResults(results) {
-    var result = results.filter(object => {
-        return object.state === "AK"
-      })
-      console.log(result);
-}
-
-function getData() {
-    const fetchUrl = "https://api.covidactnow.org/v2/states.json?apiKey=30e85e10d30e4c25886360156f029633"
-   
-    fetch(fetchUrl)
+// fetch url based on first state selection
+function handleHistoricalData() {
+  const stateDropdownSelection = document.querySelector("#stateSelect").value;
+  const fetchUrl = `https://api.covidactnow.org/v2/state/${stateDropdownSelection}.timeseries.json?apiKey=30e85e10d30e4c25886360156f029633`;
+  console.log(stateDropdownSelection);
+  var dataOne = document.querySelector("#stateSelect");
+   dataOneLabel = dataOne.options[dataOne.selectedIndex].value
+   myChart.data.datasets[0].label = dataOneLabel;
+   myChart.update();
+console.log(dataOneLabel);
+  fetch(fetchUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      displayResults(data);
-      displayStateResults(data);
+      console.log(data);
+      displayStateHistoricResults(data);
     });
 }
 
-// Event Listeners
+// display first state results
+function displayStateHistoricResults(data) {
+  const categorySelection = document.querySelector("#categorySelect").value;
+  console.log(categorySelection);
+  console.log(data.actuals.cases, data.actuals.deaths);
+  data.actualsTimeseries.forEach((result) => {
+    labels.push(result.date);
+    graphData.push(result[categorySelection]);
+    //myChart.update();
+  });
+}
 
-
-
-  const data = {
-    labels: labels,
-    datasets: [
-      {label: 'Data',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      type: "line",
-      data: graphData,},
-      {label: 'Data',
-      backgroundColor: 'blue',
-      
-      data: graphData,}
-    ]
-  };
-  
-
-
-const config = {
-    type: '',
-    data: data, 
-    options: {}
-  };
-  var myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
-// drop down menu functions
-
-
-document.querySelectorAll('.dropdown').forEach(item => {
-    item.addEventListener('click', function(event) {
-        event.stopPropagation();
-        item.classList.toggle('is-active');
+// fetch url based on second state selection
+function handleSecondData() {
+  const stateSecondDropdownSelection = document.querySelector("#stateSelectSecond").value;
+  const fetchUrl = `https://api.covidactnow.org/v2/state/${stateSecondDropdownSelection}.timeseries.json?apiKey=30e85e10d30e4c25886360156f029633`;
+  var dataTwo = document.querySelector("#stateSelectSecond")
+   dataTwoLabel = dataTwo.options[dataTwo.selectedIndex].value
+   myChart.data.datasets[1].label = dataTwoLabel;
+   myChart.update();
+  fetch(fetchUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      displayStateSecondHistoricResults(data);
     });
-});
+}
 
-getData() 
+// display second state results
+function displayStateSecondHistoricResults(data) {
+  const categorySelection = document.querySelector("#categorySelect").value;
+  console.log(categorySelection);
+  console.log(data.actuals.cases, data.actuals.deaths);
+  data.actualsTimeseries.forEach((result) => {
+    // labels.push(result.date);
+    graphSecondData.push(result[categorySelection]);
+    
+  }) ;
+}
+
+// button to submit selected data request
+var submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", handleAllData);
+
+// getData(); 
